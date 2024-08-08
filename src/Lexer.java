29d9334cb221fd014de3262a3585dd3469f92f73
Michael-u21546551 <u21546551@tuks.co.za>
2024-08-08 16:21:01 +0200

    feat: Add fileName parameter to Lexer constructor
    The Lexer class now accepts a fileName parameter in its constructor. This allows for better error reporting by including the file name in the generated tokens.
    
    Co-authored-by: [Author Name]

--- src/Lexer.java
+++ src/Lexer.java
@@ -3,10 +3,12 @@ import java.util.List;
 
 public class Lexer {
     private final String source;
+    private final String fileName;
     private char current;
     private int line, column;
 
-    public Lexer(String source) {
+    public Lexer(String source, String fileName) {
+        this.fileName = fileName;
         this.source = source;
         this.current = source.charAt(0);
         this.line = 1;
@@ -20,15 +22,84 @@ public class Lexer {
             if (token != null) {
                 tokens.add(token);
             }
+            advance();
         }
         return tokens;
     }
 
     private Token processCurrentChar() {
-        return null;
+        char c = this.current;
+        switch (c) {
+            case '(' -> {
+                return new Token(TokenType.LEFT_PAREN, fileName, String.valueOf(line), String.valueOf(column));
+            }
+            case ')' -> {
+                return new Token(TokenType.RIGHT_PAREN, fileName, String.valueOf(line), String.valueOf(column));
+            }
+            case '{' -> {
+                return new Token(TokenType.LEFT_BRACE, fileName, String.valueOf(line), String.valueOf(column));
+            }
+            case '}' -> {
+                return new Token(TokenType.RIGHT_BRACE, fileName, String.valueOf(line), String.valueOf(column));
+            }
+            case ',' -> {
+                return new Token(TokenType.COMMA, fileName, String.valueOf(line), String.valueOf(column));
+            }
+            case '.' -> {
+                return new Token(TokenType.DOT, fileName, String.valueOf(line), String.valueOf(column));
+            }
+            case '-' -> {
+                return new Token(TokenType.MINUS, fileName, String.valueOf(line), String.valueOf(column));
+            }
+            case '+' -> {
+                return new Token(TokenType.PLUS, fileName, String.valueOf(line), String.valueOf(column));
+            }
+            case ';' -> {
+                return new Token(TokenType.SEMICOLON, fileName, String.valueOf(line), String.valueOf(column));
+            }
+            case '*' -> {
+                return new Token(TokenType.STAR, fileName, String.valueOf(line), String.valueOf(column));
+            }
+            case '!' -> {
+                return new Token(match('=') ? TokenType.BANG_EQUAL : TokenType.BANG, fileName, String.valueOf(line), String.valueOf(column));
+            }
+            case '=' -> {
+                return new Token(match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL, fileName, String.valueOf(line), String.valueOf(column));
+            }
+            case '<' -> {
+                return new Token(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS, fileName, String.valueOf(line), String.valueOf(column));
+            }
+            case '>' -> {
+                return new Token(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER, fileName, String.valueOf(line), String.valueOf(column));
+            }
+            case '/' -> {
+                if (match('/')) {
+                    while (peek() != '\n' && !isAtEnd()) {
+                        advance();
+                    }
+                    return null;
+                } else {
+                    return new Token(TokenType.SLASH, fileName, String.valueOf(line), String.valueOf(column));
+                }
+            }
+            case ' ', '\r', '\t' -> {
+                return null;
+            }
+            case '\n' -> {
+                line++;
+                column = 1;
+                return null;
+            }
+            default -> {
+                return new Token(TokenType.EOF, fileName, String.valueOf(line), String.valueOf(column));
+            }
     }
 
     private boolean isAtEnd() {
         return false;
+    }
+
+    private char advance() {
+        return ' ';
     }
 }
