90cfe54d66f9c2cf0fabec9ed736be7bdb4e68e9
Ashley Kapaso <ashleykapaso@gmail.com>
2024-10-15 10:42:50 +0200

    add scope entry class

--- src/Interfaces/InnerNode.java
+++
@@ -1,8 +0,0 @@
-package Interfaces;
-
-public class InnerNode extends SyntaxTreeNode {
-    public InnerNode(String id, TokenType symbol) {
-        this.id = id;
-        this.symbol = symbol;
-    }
-}
